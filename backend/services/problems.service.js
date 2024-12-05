f327aa120bfe8f9e0c98ac960141b72fd4207c4c
AvadhootSmart <1234avadhootsmart@gmail.com>
2024-12-06 00:37:14 +0530

    reverted chromium changes

--- backend/services/problems.service.js
+++ backend/services/problems.service.js
@@ -1,63 +1,66 @@
-const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium-min");
+const puppeteer = require("puppeteer");
 const Problems = require("../models/problems");
 
-chromium.setHeadlessMode = true;
-
-chromium.setGraphicsMode = false;
-
 exports.scrapeProblemData = async (URL) => {
-
     const browser = await puppeteer.launch({
-        headless: chromium.headless, // Headless mode for serverless
-        executablePath: await chromium.executablePath(),
-        defaultViewport: chromium.defaultViewport,
-        ignoreHTTPSErrors: true,
+        headless: true,
+        // executablePath: "/usr/bin/google-chrome", // Ensure Chrome is installed
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-blink-features=AutomationControlled",
+        ],
     });
 
     const page = await browser.newPage();
 
-    // await page.setUserAgent(
-    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    // );
-    // await page.setViewport({ width: 1280, height: 800 });
-
     try {
+        await page.setUserAgent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        );
+        await page.setViewport({ width: 1280, height: 800 });
+
         await page.goto(URL, { waitUntil: "domcontentloaded" });
 
         await page.waitForSelector(".text-title-large");
         await page.waitForSelector(".text-caption");
         await page.waitForSelector('a[href^="/tag/"]');
-
-        // Extract data
+        // Extract code snippet
         const content = await page.evaluate(() => {
+            // CodeMirror stores text in spans within `.view-lines`
+            // const lines = document.querySelectorAll(".view-lines > div > span");
+            // return Array.from(lines)
+            //   .map((line) => line.innerText)
+            //   .join("\n");
+            // const code = document.querySelector(".view-lines").innerText;
             const title = document
                 .querySelector(".text-title-large")
                 .innerText.slice(3);
             const difficulty = document.querySelector(".text-caption").innerText;
             const topics = Array.from(
-                document.querySelectorAll('a[href^="/tag/"]')
+                document.querySelectorAll('a[href^="/tag/"]'),
             ).map((tag) => tag.innerText);
             return { title, difficulty, topics };
         });
 
         return { content, URL };
+        // console.log("Code:", content.code);
+        // console.log("Difficulty:", content.difficulty);
+        // console.log("Topics:", content.topics);
     } catch (err) {
-        console.error("Error while extracting data:", err);
-        throw err;
+        console.error("Error while extracting code:", err);
     } finally {
-        if (browser) {
-            await browser.close(); // Make sure the browser is closed after execution
-        }
+        await browser.close();
     }
 };
 
 exports.addProblems = async (problem) => {
     try {
+        // const newProblem = await Problems.create(problem);
         const newProblems = await Problems.insertMany(problem);
+        // Problems.insertMany(problem);
         return newProblems;
     } catch (error) {
-        console.error("Error adding problems:", error);
-        throw error;
+        console.error(error);
     }
 };
